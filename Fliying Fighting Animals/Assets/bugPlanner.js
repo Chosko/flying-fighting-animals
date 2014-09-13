bde5ce7d9a53fa7f23ee171f509c1b5255adb3eb
sfbaystudios <sfbaystudios@gmail.com>
2014-09-13 02:41:32 -0700

    I couldn't Sleep!

--- Fliying Fighting Animals/Assets/bugPlanner.js
+++ Fliying Fighting Animals/Assets/bugPlanner.js
@@ -11,12 +11,15 @@ function Start () {
 function OnMouseDown () {
 	if (gameControl.GetComponent(gameController).gamePhase == 2)						// Only active during planning phase
 	{
-		//print ("Mouse Down on " + this.name);
-		//print ("plan phase = " + planningControl.GetComponent(planningController).planPhase);
 		var bugID			= GetComponent(bugController).bugID;						// Bug ID of this bug (From other script)
-		if (planningControl.GetComponent(planningController).planPhase	== 0)			// If the planPhase is selecting a bug
-			planningControl.GetComponent(planningController).BugSelected(bugID);		// Tell planningControl that this was the bug selected
-		else if (planningControl.GetComponent(planningController).planPhase	== 1)		// If the planPhase is selecting a target
-			planningControl.GetComponent(planningController).DefendBugChosen(bugID);	// Tell planningControl that this was the bug selected TO DEFEND
+		if (!GetComponent(bugController).isEnemy)
+		{
+			if (planningControl.GetComponent(planningController).planPhase	== 0)			// If the planPhase is selecting a bug
+				planningControl.GetComponent(planningController).BugSelected(bugID);		// Tell planningControl that this was the bug selected
+			else if (planningControl.GetComponent(planningController).planPhase	== 1)		// If the planPhase is selecting a target
+				planningControl.GetComponent(planningController).DefendBugChosen(bugID);	// Tell planningControl that this was the bug selected TO DEFEND
+		}
+		else if (planningControl.GetComponent(planningController).planPhase	== 1)
+			planningControl.GetComponent(planningController).AttackBugChosen(bugID);		// This is an enemy Bug, so only works if player is choosing to attack it
 	}
 }
--- Fliying Fighting Animals/Assets/planningController.js
+++ Fliying Fighting Animals/Assets/planningController.js
@@ -5,9 +5,12 @@ var planPhase		: int				= 0;				// 0 = choosing bug, 1 = choosing target
 var currentBug		: GameObject;							// Current bug setting it's target
 var gameControl		: GameObject;							
 var clickLayerMask	: LayerMask;							// Set to only "Primitives" layer
+var playerFlag		: GameObject;
+var tempEnemyObj	: GameObject;
 
 function Start () {
 	gameControl 	= GameObject.Find("Game Control");		// Set this object
+	playerFlag		= GameObject.Find("Player Flag");
 }
 
 function Update () {
@@ -32,7 +35,6 @@ function DefendBugChosen(bugID : int){
 function PatrolPosChosen(mapPos : Vector3){
 	planText.GetComponent(UI.Text).text = "Click A Bug";							// Change the text to tell the player what to do
 	mapPos.y = currentBug.transform.position.y;
-	print ("mapPos: " + mapPos);
 	currentBug.GetComponent(bugController).targetSpot = mapPos;						// Set the targetSpot for selected bug to the chosen map Vector3 (Position)
 	planPhase		= 0;			// Reset planPhase
 }
@@ -42,3 +44,42 @@ function AttackBugChosen(bugID : int){				// Pass along the ENEMY Bug ID
 	currentBug.GetComponent(bugController).targetObject = gameControl.GetComponent(gameController).enemyBug[bugID]; // Set the targetObject for selected bug to the chosen bug object
 	planPhase		= 0;			// Reset planPhase
 }
+
+function FlagChosen(flagObject : GameObject){
+	planText.GetComponent(UI.Text).text = "Click A Bug";							// Change the text to tell the player what to do
+	currentBug.GetComponent(bugController).targetObject = flagObject; // Set the targetObject for selected bug to the chosen bug object
+	planPhase		= 0;			// Reset planPhase
+}
+
+function FillInBlanks(playerID : int){
+	var tempPlayerBug = gameControl.GetComponent(gameController).playerBug[playerID];
+	if (!tempPlayerBug.GetComponent(bugController).targetObject && tempPlayerBug.GetComponent(bugController).targetSpot == Vector3(0,0,0))
+		tempPlayerBug.GetComponent(bugController).targetObject = GameObject.Find("Enemy Flag");
+}
+
+function EnemyChooseAttack(enemyID : int, playerID : int){
+	tempEnemyObj = gameControl.GetComponent(gameController).enemyBug[enemyID];
+	tempEnemyObj.GetComponent(bugController).targetObject = gameControl.GetComponent(gameController).playerBug[playerID];
+}
+
+function EnemyChoosePatrol(enemyID : int, posObj : GameObject){
+	tempEnemyObj = gameControl.GetComponent(gameController).enemyBug[enemyID];
+	tempEnemyObj.GetComponent(bugController).targetSpot	= posObj.transform.position;
+	tempEnemyObj.GetComponent(bugController).targetSpot.y = gameControl.GetComponent(gameController).enemyBug[enemyID].transform.position.y;
+}
+
+function EnemyChooseDefend(enemyID : int, otherEnemyID : int){
+	tempEnemyObj = gameControl.GetComponent(gameController).enemyBug[enemyID];
+	tempEnemyObj.GetComponent(bugController).targetObject = gameControl.GetComponent(gameController).enemyBug[otherEnemyID];
+}
+
+function EnemyChooseFlag(enemyID : int){
+	tempEnemyObj = gameControl.GetComponent(gameController).enemyBug[enemyID];
+	tempEnemyObj.GetComponent(bugController).targetObject = playerFlag;
+}
+
+
+
+
+
+
